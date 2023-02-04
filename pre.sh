0afe80fafb7a1fd567950e0c287de4a0be663f49
EBK21 <chkd13303@gmail.com>
2023-02-05 00:32:47 +0800

    update mingw crt build

--- pre.sh
+++ pre.sh
@@ -15,7 +15,7 @@ fi
 git clone https://github.com/eebssk1/m_gcc; checkreturn $?
 git clone https://github.com/eebssk1/m_binutils; checkreturn $?
 
-wget "https://github.com/eebssk1/mingw-crt-build/releases/download/455c5ff7/mingw-crt.tgz"
+wget "https://github.com/eebssk1/mingw-crt-build/releases/download/5c897cf5/mingw-crt.tgz"
 tar -xf mingw-crt.tgz
 rm -rf mingw-crt.tgz
 
