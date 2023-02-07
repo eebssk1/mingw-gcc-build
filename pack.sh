4cbf1e5ccf6c80f99956efd96ff6001ece449270
EBK21 <chkd13303@gmail.com>
2023-02-07 23:51:07 +0800

    strip toolchain binary

--- pack.sh
+++ pack.sh
@@ -17,4 +17,14 @@ cd ..
 
 git log -7 > mingw-gcc/mreplog.txt
 
+find mingw-gcc -type f \( -name cc1* -or -name collect2 -or -name f951 -or -name lto1 -or -name lto-wrapper -or -name gengtype -or -name fixincl \) -exec strip {} \;
+for f in mingw-gcc/*/bin/* mingw-gcc/*/*/bin/*
+do
+if [ "x$(basename $f | grep .dll)" != "x" ]
+then
+continue
+fi
+strip $f
+done
+
 tar --gzip -cf mingw-gcc.tgz mingw-gcc
